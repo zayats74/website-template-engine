18b6a0ccccd6e2696767515f3d2e597f3d82b972
Zayats Natalia <n.zayats74@gmail.com>
2025-04-20 23:59:00 +0400

    changed id generation in user entity

--- src/main/java/com/example/websitetemplateengine/entity/User.java
+++ src/main/java/com/example/websitetemplateengine/entity/User.java
@@ -5,6 +5,7 @@ import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Getter
@@ -15,8 +16,8 @@ import java.time.LocalDateTime;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     @Column(unique = true, nullable = false)
     private String username;
